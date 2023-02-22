5ff564df575b9bce0d9bda6038c632a621782937
Manoj Kumar <v-maku23@microsoft.com>
2023-02-22 16:10:27 +0530

    adding project

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World! Main");
+newmethod();
+
+void newmethod() 
+{
+Console.WriteLine("newmethod");
+ Console.WriteLine("newmethod");
+    int i;
+    i = 0;
+    i = i + 1;
+    Console.WriteLine("newmethod + " + i.ToString());
+}
